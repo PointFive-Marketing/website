32b7b1f2a1657b89e305b7e7bacbb69750513d32
PointFive-Marketing <dave.anderson@Pointfive.co>
2026-02-20 09:49:03 -0500

    Adjust logo sizes for Accenture and Presidio

--- partner-loader.js
+++ partner-loader.js
@@ -602,10 +602,10 @@
                             <img src="https://cdn.prod.website-files.com/664c5d9fee3e617bc2829b44/699864411ee461dc0f2c7bf3_Capgemini-Logo.wine.svg" alt="Capgemini" style="max-width: 100%; height: auto;">
                         </div>
                         <div class="partner-logo">
-                            <img src="https://cdn.prod.website-files.com/664c5d9fee3e617bc2829b44/6998644121c6c39905b3fae2_Accenture_logo.svg" alt="Accenture" style="max-width: 70%; height: auto;">
+                            <img src="https://cdn.prod.website-files.com/664c5d9fee3e617bc2829b44/6998644121c6c39905b3fae2_Accenture_logo.svg" alt="Accenture" style="max-width: 60%; height: auto;">
                         </div>
                         <div class="partner-logo">
-                            <img src="https://cdn.prod.website-files.com/664c5d9fee3e617bc2829b44/699864417283bfd8ea0eb7d4_a773d4f6e6b179c71df20da61eb62afc_Presidio_logo%20%281%29.png" alt="Presidio" style="max-width: 100%; height: auto;">
+                            <img src="https://cdn.prod.website-files.com/664c5d9fee3e617bc2829b44/699864417283bfd8ea0eb7d4_a773d4f6e6b179c71df20da61eb62afc_Presidio_logo%20%281%29.png" alt="Presidio" style="max-width: 90%; height: auto;">
                         </div>
                     </div>
                 </div>
